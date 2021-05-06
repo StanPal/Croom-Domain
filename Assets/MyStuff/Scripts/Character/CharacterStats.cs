f4cb72ef54a4ddc124668144f6856da927754c6a
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2021-05-05 22:43:12 -0700

    -Created Healthbars and instantiated cubes in opposite positions when client joins room

--- Assets/MyStuff/Scripts/Character/CharacterStats.cs
+++ Assets/MyStuff/Scripts/Character/CharacterStats.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    private float _characterMaxHealth;
     [SerializeField] private string _characterName;
-    [SerializeField] private float _characterHealth;
-    [SerializeField] private float _characterDamage; 
-   
+    [SerializeField] private float _characterHealth = 100f;
+    [SerializeField] private float _characterDamage = 10f;
+
+
+    public void Start()
+    {
+        _characterMaxHealth = _characterHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _characterHealth -= damage;
+    }
 }
--- Assets/MyStuff/Scripts/Manager/SpawnManager.cs
+++ Assets/MyStuff/Scripts/Manager/SpawnManager.cs
@@ -2,15 +2,20 @@ using Photon.Pun;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviourPun
-{
-    [SerializeField] private GameObject _cube; 
+{    
+    [SerializeField] private Transform _player1Pos;
+    [SerializeField] private Transform _player2Pos; 
 
     // Start is called before the first frame update
     void Start()
     {
         if(PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.Instantiate("Cube", new Vector3(0, 2.5f, 0), Quaternion.identity);
+            PhotonNetwork.Instantiate("Cube", _player1Pos.position, Quaternion.identity);
+        }
+        else
+        {
+            PhotonNetwork.Instantiate("Cube", _player2Pos.position, Quaternion.identity);
         }
     }
 }
